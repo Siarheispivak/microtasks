e073f9d44686d29837426e2eb3586b0c313fff51
Siarheispivak <siarheispivak2@gmail.com>
2022-11-13 17:42:42 +0300

    newComponent need refactor!

--- microtasks/src/App.tsx
+++ microtasks/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {Header} from "./site/Header";
 import {Body} from "./site/Body";
 import {Footer} from "./site/Footer";
+import {NewComponent} from "./site/NewComponent";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header titleForHeader={'NEW HEADER'}/>
         <Body titleForBody={'NEW BODY'}/>
         <Footer titleForFooter={'NEW FOOTER'}/>
+        <NewComponent />
     </div>
   );
 }
