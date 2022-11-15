c1be6df62f0d28280af0f5e710e39647cbddee6c
Siarheispivak <siarheispivak2@gmail.com>
2022-11-15 21:53:26 +0300

    finished useState microtask

--- microtasks/src/App.tsx
+++ microtasks/src/App.tsx
@@ -1,32 +1,24 @@
-import React,{ MouseEvent } from 'react';
+import React, {MouseEvent, useState} from 'react';
 
 import './App.css';
-import {NewComponent} from "./site/NewComponent";
-import {Button} from "./components/Button";
+
 
 
 function App() {
+let [a,setA] = useState(1)
+const onClickHandler = () => {
+    setA(++a)
+}
+    const onClickHandler2 = () => {
+        setA(0)
+    }
 
-    const Button1Foo=(subscriber: string,age:number,address:string)=>{
-        console.log(subscriber,age)
-    }
-    const Button2Foo=(subscriber: string)=>{
-        console.log(subscriber)
-    }
-    const Button3Foo=()=>{
-        console.log('i am stupid')
-    }
 
     return (
     <div className="App">
-
-        {/*<button >MyYouTubeChanel-1</button>*/}
-        {/*<button >MyYouTubeChanel-2</button>*/}
-        <Button name={'MyYouTubeChanel-1'} callBack={()=>Button1Foo('i am Vasilij', 21, 'live in minsk')}/>
-        <Button name={'MyYouTubeChanel-2'}callBack={()=>Button2Foo('i am Ivan')}/>
-        {/*<Button name={'MyYouTubeChanel-3'}callBack={()=>Button2Foo('i am stupid button')}/>*/}
-        <Button name={"Stupid"} callBack={Button3Foo}/>
-
+        <h1>{a}</h1>
+        <button onClick={onClickHandler}>numbers</button>
+        <button onClick={onClickHandler2}>0</button>
     </div>
   );
 }
